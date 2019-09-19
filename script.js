55248e4e113a4f743d403f1118ddfd55a1bea4c3
Dan Erbland <danerbland@gmail.com>
2019-09-19 10:35:05 -0400

    started workshop

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your code here
+
+const table = document.getElementById('table');
+
+makeRow = () => {
+  const newTr = document.createElement('tr');
+  for (let i = 0; i < 20; i ++){
+    const newTd = document.createElement('td');
+    newTr.append(newTd);
+  }
+  table.append(newTr);
+}
+
+makeRow();
+makeRow();
+makeRow();
