ff8cd4b3ed520f73bd33b54c65782e35f29ee60b
David-Sanvicente <sanvicented1@gmail.com>
2019-09-19 12:27:11 -0400

    created mousedown/over/up listeners (currently broken)

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Your code here
+let isDrawing = false;
+let boxColor = 'red';
+// let boxColor;
 
 const table = document.getElementById('table');
 
@@ -19,12 +22,13 @@ const button = document.getElementById('add-row');
 button.addEventListener('click', makeRow);
 
 function colorize (event) {
+  isDrawing = true;
   let tableCell = event.target;
 
-  if(tableCell.className.length){
+  if(tableCell.className.length || tableCell.tagName !== 'TD'){
     tableCell.className = '';
   } else {
-    tableCell.className = 'green';
+    tableCell.className = boxColor;
   }
   // if(tableCell.className === ''){
   //   tableCell.className = 'green';
@@ -33,5 +37,21 @@ function colorize (event) {
   // }
 }
 
-table.addEventListener('click', colorize);
+table.addEventListener('mousedown', colorize);
 
+table.addEventListener('mouseover', function(event){
+  if(isDrawing){
+    colorize(event);
+    // colorize();
+  }
+})
+
+// table.addEventListener('mouseup', isDrawing = false)
+
+function changeColor(event){
+  // console.log(event.target.value);
+   boxColor = event.target.value;
+}
+
+const selector = document.getElementsByTagName("select")[0];
+selector.addEventListener('change', changeColor);
